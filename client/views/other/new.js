9fc1440598e9bb49fa6b3d17d796241ee6af1d53
pyankoff <pyankoff@gmail.com>
2015-09-28 10:55:32 +0700

    new class or person notes

--- client/views/other/new.js
+++ client/views/other/new.js
@@ -1,3 +1,9 @@
+Template.newNote.helpers({
+  tag: function(){
+    return FlowRouter.getQueryParam("tag")
+  }
+});
+
 Template.newNote.onCreated(function() {
   var self = this;
   self.autorun(function() {
